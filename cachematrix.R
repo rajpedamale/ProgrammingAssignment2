463ad26223c22fb43290fcef1c1201bfe9e0f828
Rajesh Pedamale <pedamale@gmail.com>
2015-07-23 06:59:51 +1000

    Added an Object that encapsulates matrix and an inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions in this file help calculate inverse of the given
+## matix and cache the results.
 
-## Write a short comment describing this function
+## makeCacheMatrix is an object that contains the matrix
+## and its inverse.
+## It provides getters and setters for the matrix and the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        s <- NULL
 
+        set <- function(y) {
+                x <<- y
+                # Reset inverse when the matrix changes
+                s <<- NULL 
+        }
+        get <- function() x
+        setSolve <- function(inverse) i <<- inverse
+        getSolve <- function() m
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
 }
 
 
